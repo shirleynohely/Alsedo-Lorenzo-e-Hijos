a9a8ff069ab69e729dc46108d46ea7f6087071c9
Leyly <nohely.ramosderosas@gmail.com>
2022-06-07 23:06:25 -0300

    primer commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+
+  const links = document.querySelector(".nav-links");
+  const list = document.querySelector(".image-list");
+  
+  list.addEventListener("click", () => {links.classList.toggle('show-links')})
+  
